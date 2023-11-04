5019082ba8aa4eb956f50e90341f6ef447264685
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2023-11-03 19:36:10 -0600

    C# Prep 3—Loops

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,39 @@
+//C# Prep 3—Loops
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        //Console.WriteLine($"What is the magic number?");
+        //int magiNum = int.Parse(Console.ReadLine()) ;
+
+        //Random Mode
+        Random randomGen = new Random();
+        int magiNum = randomGen.Next(1, 101);
+       
+        int guess = -1;
+
+        while (guess != magiNum)
+        {
+            Console.Write($"What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magiNum > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magiNum < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Good job, You have guess it!");
+            }
+        }
+
+
     }
 }
