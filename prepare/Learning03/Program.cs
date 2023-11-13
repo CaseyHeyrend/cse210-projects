ac754d81648a74ab61c31a24255452f28d6e96bc
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2023-11-13 15:07:20 -0700

    comments

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -12,17 +12,17 @@ class Program
         Console.WriteLine(f1.GetDecimalVaule());
 
         Fraction f2 = new Fraction(5);
-        Console.WriteLine(f2.GetFractionString());
-        Console.WriteLine(f2.GetDecimalVaule());
+        Console.WriteLine(f2.GetFractionString()); // Copy and Paste from line 11 Change f#
+        Console.WriteLine(f2.GetDecimalVaule()); // Copy and Paste from line 12 Change f#
 
         // Step 6
         Fraction f3 = new Fraction(3, 4);
-        Console.WriteLine(f3.GetFractionString());
-        Console.WriteLine(f3.GetDecimalVaule());
+        Console.WriteLine(f3.GetFractionString()); // Copy and Paste from line 11 Change f#
+        Console.WriteLine(f3.GetDecimalVaule()); // Copy and Paste from line 12 Change f#
 
         Fraction f4 = new Fraction(1, 3);
-        Console.WriteLine(f4.GetFractionString());
-        Console.WriteLine(f4.GetDecimalVaule());
+        Console.WriteLine(f4.GetFractionString()); // Copy and Paste from line 11 Change f#
+        Console.WriteLine(f4.GetDecimalVaule()); // Copy and Paste from line 12 Change f#
 
     }
 }
