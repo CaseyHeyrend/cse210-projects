54aaa435d6b56cf0e7d444fc8862d70792a20d7c
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2023-12-07 22:10:49 -0700

    The Video and Comments are Real

--- final/Foundation1/Comments.cs
+++ final/Foundation1/Comments.cs
@@ -1,1 +1,22 @@
 using System;
+
+public class Comment
+{
+    //Attributes 
+    private string _name;
+    private string _text;
+    
+    //Constructors
+    public Comment(string name, string text)
+    {
+        _name = name;
+        _text = text;
+
+    }
+    //Methods
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"{_name} - {_text}");
+    }
+}
--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -5,6 +5,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        //Console.WriteLine("Hello Foundation1 World!");
+        List<Video> vidsList = new List<Video>();
+
+        //Video 1
+        Video vid1 = new Video("Who is Grace Hopper? Meet the Queen of Code", "HoneyPot", 608);//Real Video
+        Comment vid1Comment1 = new Comment("mohamedaityoussef9965", "this is great, i love the editing and this a great story, as CS student i'm forever grateful for grace hopper");
+        Comment vid1Comment2 = new Comment("whoisryosuke", "Really good energy + inspo for the morning. Great video!");
+        Comment vid1Comment3 = new Comment("Climacticc_Chaos", "Kinda ready to hear how she uses the compiler to translate words into binary across transistor logic gates");
+
+        vid1.ListComment(vid1Comment1);
+        vid1.ListComment(vid1Comment2);
+        vid1.ListComment(vid1Comment3);
+
+        vidsList.Add(vid1);
+
+        //Video 2
+        Video vid2 = new Video("", "", 300);
+        Comment vid2Comment1 = new Comment("Adam", "It is Good");
+        Comment vid2Comment2 = new Comment("Adam", "It is Good");
+        Comment vid2Comment3 = new Comment("Adam", "It is Good");
+
+        vid2.ListComment(vid2Comment1);
+        vid2.ListComment(vid2Comment2);
+        vid2.ListComment(vid2Comment3);
+
+        vidsList.Add(vid2);
+
+        //Video 3
+        Video vid3 = new Video("", "", 403);
+
+        Comment vid3Comment1 = new Comment("Adam", "It is Good");
+        Comment vid3Comment2 = new Comment("Adam", "It is Good");
+        Comment vid3Comment3 = new Comment("Adam", "It is Good");
+
+        vid3.ListComment(vid3Comment1);
+        vid3.ListComment(vid3Comment2);
+        vid3.ListComment(vid3Comment3);
+
+        vidsList.Add(vid3);
+
+        foreach (Video video in vidsList)
+        {
+            video.DisplayInfo();
+            Console.WriteLine();
+            Console.WriteLine();
+            
+        }
+        
     }
 }
