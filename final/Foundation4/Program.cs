a42f437c69f7f14d4f17230072b41c2a2c60e8e9
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2023-12-09 22:08:18 -0700

    hmmmmm

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -5,6 +5,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        //Console.WriteLine("Hello Foundation4 World!");
+        List <Exercise> exeList = new List<Exercise>();
+        
+        Running running = new Running("June 17, 2024", 30, 5.8);
+        exeList.Add(running);
+
+        Cycling stationaryBicycles = new Cycling("June 19, 2024", 60, 15);
+        exeList.Add(stationaryBicycles);
+        
+        Swimming swimlaps = new Swimming("June 21, 2024", 20, 20);
+        exeList.Add(swimlaps);
+
+        Console.WriteLine("Exercises: ");
+        Console.WriteLine();
+
+        foreach (Exercise exercise in exeList)
+        {
+            Console.WriteLine(exercise.GetSummary());
+        }
     }
 }
