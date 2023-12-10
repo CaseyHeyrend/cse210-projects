d6cb019861c2eff1b36c85d4a18a9e466d85d7ba
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2023-12-09 18:12:24 -0700

    Update OutdoorGatherings.cs

--- final/Foundation3/OutdoorGatherings.cs
+++ final/Foundation3/OutdoorGatherings.cs
@@ -1,1 +1,44 @@
 using System;
+
+public class OutdoorGathering : Event
+{
+    //Attributes
+    private string _weatherFor;//weatherForecast
+    //Constructors
+    public OutdoorGathering(string title, string description, string date, string time, Address address, string weatherFor) : base(title, description, date, time, address)
+    {
+        _weatherFor = weatherFor;
+    }
+
+    //Methods
+    public string GetWeather()
+    {
+        return _weatherFor;
+    }
+    public string GenerateWeatherForecast()
+    {
+        return "The weather will be " + _weatherFor + ".";
+    }
+    public string ReturnEventType()
+    {
+        return "Outdoor Gathering";
+    }
+    public string GenerateDetailOG()//GetFullDetails //Outdoor Gathering
+    {
+        string detailed = "";
+        string weatherStatement = GenerateWeatherForecast();
+
+        detailed += GenerateStandardDet() + "\n";
+        detailed += "Statement of the Weather: " + weatherStatement;
+        return detailed;
+    }
+    public string GenerateShortOG()//Outdoor Gathering
+    {
+        string shortOG = "";
+        string eventType = ReturnEventType();
+
+        shortOG += "Event Type: " + eventType + "\n";
+        shortOG += GenerateShortDes();
+        return shortOG;
+    }
+}
