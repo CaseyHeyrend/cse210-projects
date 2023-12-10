135a1d957e97892a96e0c3b41513012962326229
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2023-12-09 18:16:20 -0700

    space

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -10,6 +10,7 @@ class Program
 
         //Video 1
         Video vid1 = new Video("How to care for trees", "Meadow Le", 310);
+        
         Comment vid1Comment1 = new Comment("Uriah Waters", "I didn't know there was so much info for taking care of trees.");
         Comment vid1Comment2 = new Comment("Emmy Bean", "The knowledge of taking care of something is incredible to know about.");
         Comment vid1Comment3 = new Comment("Grant Montoya", "Picking where the tree is going to go is important and stay on the decision for the tree.");
@@ -22,6 +23,7 @@ class Program
 
         //Video 2
         Video vid2 = new Video("What not to do with trees care", "Molly Carpenter", 500);
+
         Comment vid2Comment1 = new Comment("Ross Erickson", "Oh, trees need lots of water to live. Note to self.");
         Comment vid2Comment2 = new Comment("Miley Potter", "Spacing for the tree so it can grow and trimming is good as well.");
         Comment vid2Comment3 = new Comment("Cale Goodman", "Sometimes you gonna make a mistake and take a mulch needed a break.");
@@ -34,6 +36,7 @@ class Program
 
         //Video 3
         Video vid3 = new Video("What you need to care for trees guide", "Coby Bush", 603);
+
         Comment vid3Comment1 = new Comment("Heath Paul", "Trees can get sick!");
         Comment vid3Comment2 = new Comment("Karen Bell", "There is tree food. Is it the same as plant food?");
         Comment vid3Comment3 = new Comment("Jeremy Blender", "There are many different products, and some are quite similar to each other.");
