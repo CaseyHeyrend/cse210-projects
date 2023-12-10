d79f8d5e554a0ad7805de2cdd7efb0481987b6e7
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2023-12-09 18:09:26 -0700

    Update Receptions.cs

--- final/Foundation3/Receptions.cs
+++ final/Foundation3/Receptions.cs
@@ -1,1 +1,37 @@
 using System;
+
+public class Receptions : Event
+{
+    //Attributes
+    private string _rsvpEmail;
+    //Constructors
+    public Receptions(string title, string description, string date, string time, Address address, string rsvpEmail) : base(title, description, date, time, address)
+    {
+        _rsvpEmail = rsvpEmail;
+    }
+    //Methods
+     public string GetRsvpEmail()
+     {
+        return _rsvpEmail;
+     }
+
+     public string ReturnEventType()
+     {
+        return "Receptions";
+     }
+     public string GenerateDetailReception()
+     {
+        string detailed = GenerateStandardDet() + "\n";
+        detailed += "RSVP: " + _rsvpEmail;
+        return detailed;
+     }
+     public string GenerateShortReception()
+     {
+      string shortReception = "";
+      string eventType = ReturnEventType();
+      shortReception += "Event Type: " + eventType + "\n";
+      shortReception += GenerateShortDes();
+      return shortReception;
+     }
+    
+}
