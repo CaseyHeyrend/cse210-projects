0756cf2050bd3dadf097fbd47c0d08a088f8233d
Casey Heyrend <caseyheyrend.byui8@gmail.com>
2023-12-09 18:11:39 -0700

    Update Program.cs

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -5,6 +5,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        //Console.WriteLine("Hello Foundation3 World!");
+       //Console.WriteLine("Hello Foundation3 World!");
+        //"2014 Lady Bug Drive", "Queens", "NY", "USA"
+        //"4958 Cambridge Place", "Lutz", "FL", "USA"
+        //"2726 Sunset Drive", "Pueblo", "CO", "USA"
     }
 }
